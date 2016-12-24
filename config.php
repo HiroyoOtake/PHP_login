5deb0815da28c2fc66ae341db8613addbe84d5ee
otake <otake.hiroyo@gmail.com>
2016-12-24 11:25:39 +0900

    設定ファイルの準備

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// DB接続に必要な情報
+define('DSN', 'mysql:host=localhost;dbname=nowall_login;charset=utf8');
+define('USERS', 'root' );
+define('PASSWORD', 'root');
+
+// エラーレベルの設定
+error_reporting(E_ALL & ~E_NOTICE);
