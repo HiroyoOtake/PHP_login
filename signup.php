375416153630762f75d8c0a092999f583da5ecea
otake <otake.hiroyo@gmail.com>
2016-12-24 12:20:29 +0900

    バリデーションの設定

--- signup.php
+++ signup.php
@@ -4,7 +4,28 @@ require_once('config.php');
 require_once('functions.php');
 
 session_start;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$name = $_POST['name'];
+	$password = $_POST['password'];
+
+	$error = array();
+
+	// バリデーション
+	if ($name == '')
+	{
+		$errors['name'] = 'ユーザネームが未入力です';
+	}
+
+	if ($password == '')
+	{
+		$errors['password'] = 'パスワードが未入力です';
+	}
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -14,8 +35,16 @@ session_start;
 <body>
 	<h1>新規登録画面です!</h1>
 	<form action="" method="post">
-		ユーザーネーム: <input type="text" name="name"><br>
-		パスワード: <input type="text" name="password"><br>
+		ユーザーネーム: <input type="text" name="name">
+		<?php if ($errors['name']): ?>
+			<?php echo h($errors['name']) ?>
+		<?php endif; ?>
+		<br>
+		パスワード: <input type="text" name="password">
+		<?php if ($errors['password']): ?>
+			<?php echo h($errors['password']) ?>
+		<?php endif; ?>
+		<br>
 		<input type="submit" value="ログイン">
 	</form>
 	<a href="login.php">ログインはこちら</a>
